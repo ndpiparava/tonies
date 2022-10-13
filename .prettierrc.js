59f9e56297ea08520463cd6c377dddbebf1c47ac
ndpiparava <ndpiparava@gmail.com>
2022-10-13 10:54:06 +0200

    Added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  //jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
